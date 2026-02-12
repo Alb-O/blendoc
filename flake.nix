005fca8762b982625b7d5adb85b02058f454dc29
Albert O'Shea <albertoshea2@gmail.com>
2026-02-12 14:55:01 +1100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Experimental Blender tools";
+
+  inputs = {
+    self.submodules = true;
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: import ./nix/default.nix inputs;
+}
